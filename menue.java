251e31361306ef473f153d04ec67e30f9bf1e359
Michael Sauer <michael.sauer@htwsaar.de>
2017-01-29 11:18:00 +0100

    Initialer Stand V1.0 vom 29.01.2017

---
+++ menue.java
@@ -0,0 +1,45 @@
+/*
+ * Dieses kleine Java-Programm erzeugt ein sehr einfaches
+ * Konsolen-Menü und dient dazu die Arbeitsweise eines RCS
+ * zu erklären.
+ * Autor(en): Michael Sauer
+ * Datum:     03.01.2014
+ * Datum:     28.11.2014 //fuer Veraenderung zu demonstrieren
+ * Compile:   javac menue.java
+ * Execute:   java menu
+ */
+
+import java.util.Scanner;
+
+public class menue{
+        public static void main(String[] args) {
+        	final int PROGRAMMENDE=0;
+        	final int MICHAEL_SAUER=1;
+		final int MIKE_SOUR=2;
+        	boolean isEnde=false;
+
+        	String[] textfeld={"Ciao",
+                                "Michael Sauer",
+                                "Mike Sour"};
+
+        	while(!isEnde){
+                	Scanner scanner = new Scanner(System.in);
+                	int wahl = scanner.nextInt();
+                	switch (wahl) {
+                        	case PROGRAMMENDE:
+                                	System.out.println(textfeld[0]);
+                                	isEnde=true;
+                        	break;
+                        	case MICHAEL_SAUER:
+                                	System.out.println("Michael Sauer");
+                        	break;
+                        	case MIKE_SOUR:
+                                	System.out.println("Mike Sour");
+                        	break;
+                        	default:
+                                	System.out.println("Fehler: Kenne ich nicht!");
+                        	}
+                	}
+        	}
+	}
+
