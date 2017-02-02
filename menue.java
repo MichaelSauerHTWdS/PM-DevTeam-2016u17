28f887bd5ee5430f5bbdaddb1a376c3bfdc30736
Jernsus <jens-rolf@live.de>
2017-02-02 14:22:49 +0100

    Namen

--- menue.java
+++ menue.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * Dieses kleine Java-Programm erzeugt ein sehr einfaches
  * Konsolen-Menü und dient dazu die Arbeitsweise eines RCS
  * zu erklären.
@@ -23,6 +23,7 @@ public class menue{
                                 "Mike Sour"};
 
         	while(!isEnde){
+			System.out.println("Geben sie 1 oder 2 ein, Eingabe von 0 beendet das Programm: ");
                 	Scanner scanner = new Scanner(System.in);
                 	int wahl = scanner.nextInt();
                 	switch (wahl) {
@@ -31,10 +32,10 @@ public class menue{
                                 	isEnde=true;
                         	break;
                         	case MICHAEL_SAUER:
-                                	System.out.println("Michael Sauer");
+                                	System.out.println(textfeld[1]);
                         	break;
                         	case MIKE_SOUR:
-                                	System.out.println("Mike Sour");
+                                	System.out.println(textfeld[2]);
                         	break;
                         	default:
                                 	System.out.println("Fehler: Kenne ich nicht!");
