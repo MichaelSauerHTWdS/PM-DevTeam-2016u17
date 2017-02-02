7ba0a450374f638d388af080ba7555bf667a5fb0
cRsLoch <whatsrightnow@gmx.de>
2017-02-02 14:41:22 +0100

    Update

--- menue.java
+++ menue.java
@@ -19,7 +19,7 @@ public class menue{
             final int   MATHIAS_W       =3;
             final int   BANANE          =4;
             final int   APFEL           =5;
-	    final int 	MARC		=6;
+		
 		
 		final int NAME_DER_COOLEN_TYPEN = 19032;
         	boolean isEnde=false;
@@ -31,25 +31,28 @@ public class menue{
 
         	while(!isEnde){
                 	Scanner scanner = new Scanner(System.in);
-			System.out.println("Bitte geben sie eine Zahl ein");
+			System.out.println("Geben sie eine Zahl ein:");
                 	int wahl = scanner.nextInt();
                 	switch (wahl) {
                         	case PROGRAMMENDE:
                                 	System.out.println(textfeld[0]);
                                 	isEnde=true;
                         	break;
+					
                         	case MICHAEL_SAUER:
                                 	System.out.println("Michael Sauer");
                         	break;
+					
                         	case MIKE_SOUR:
                                 	System.out.println("Mike Sour");
                         	break;
-							case NAME_DER_COOLEN_TYPEN:
-								System.out.println("Klaus,Andreas,Dennis");
-							break;
+					
+				case NAME_DER_COOLEN_TYPEN:
+					System.out.println("Klaus,Andreas,Dennis");
+				break;
                             
-                            case MATHIAS_W:
-                                System.out.println("Mathias Wittling");
+                            	case MATHIAS_W:
+                	                System.out.println("Mathias Wittling");
 				break;
                             
 				case BANANE:
@@ -61,8 +64,6 @@ public class menue{
 					System.out.println("Banane hat recht!!!");
                             
 				break;
-				case MARC:
-					System.out.println("Hier ist Marc");
                         	default:
                                 	System.out.println("Fehler: Kenne ich nicht!");
                             
