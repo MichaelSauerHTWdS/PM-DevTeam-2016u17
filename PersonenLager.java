6c653c0dc60323e37eec44fc361a4776265a9615
Fabian Stiewitz <fabian.stiewitz@gmail.com>
2017-02-02 14:17:08 +0100

    Add PersonenLager(temp)

---
+++ PersonenLager.java
@@ -0,0 +1,64 @@
+public class PersonenLager implements Iterable<Person> {
+
+    private Person personen[];
+    private int size;
+    private int current;
+
+    public PersonenLager(int s) {
+        size = s;
+        current = 0;
+        personen = new Person[s];
+    }
+
+    public Person get(String name) {
+        if(current == 0)
+            return null;
+        for(Person p : personen)
+            if(p.getName() == name)
+                return p;
+        return null;
+    }
+
+    public Person add(Person p) {
+        if(p == null)
+            throw new Exception("Person ist null");
+        if(size == current)
+            throw new Exception("PersonenLager voll");
+        personen[current++] = p;
+        return p;
+    }
+
+    public Person remove(String name) {
+        Person t = null;
+        for(int i = 0; i < current; ++i) {
+            if(personen[i].getName() == name) {
+                t = personen[i];
+                for(int j = i; j < size - 1; ++j) {
+                    personen[j] = personen[j + 1];
+                }
+                personen[size - 1] = null;
+                current = current - 1;
+                return t;
+            }
+        }
+        return null;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int getCurrent() {
+        return currrent;
+    }
+
+    public Person getByIndex(int i) {
+        return personen[i];
+    }
+
+    public PersonenLagerIterator iterator() {
+        return new PersonenLagerIterator(this);
+    }
+
+}
+
