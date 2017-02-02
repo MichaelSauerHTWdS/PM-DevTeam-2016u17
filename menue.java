66cd9c90dd91711158c97e998ac6dc13c572af92
localhostPIB <obosslet@htwsaar.de>
2017-02-02 14:13:14 +0100

    Update menue.java

--- menue.java
+++ menue.java
@@ -13,13 +13,13 @@ import java.util.Scanner;
 
 public class menue{
         public static void main(String[] args) {
-        	final int PROGRAMMENDE=0;
-        	final int MICHAEL_SAUER=1;
-		final int MIKE_SOUR=2;
-		final int MATHIAS_W=3;
-		final int BANANE=4;
-		final int APFEL=5;
-        	boolean isEnde=false;
+        	final int PROGRAMMENDE			=0;
+        	final int MICHAEL_SAUER			=1;
+		final int MIKE_SOUR			=2;
+		final int MATHIAS_W			=3;
+		final int BANANE			=4;
+		final int APFEL				=5;
+        	boolean isEnde				=false;
 
         	String[] textfeld={"Ciao",
                                 "Michael Sauer",
